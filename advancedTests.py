ae80cceddcfe0addaa478c2698f6f3370a30eec5
rohaankhalid <rohaankhalid10@gmail.com>
2024-05-31 18:03:38 -0500

    added setup function, removed test for natural log

--- advancedTests.py
+++ advancedTests.py
@@ -1,28 +1,26 @@
 import pytest
-import math
 from src.calculator import Calculator
 
-def test_sine():
+def setup_function():
+    global calc
     calc = Calculator()
+
+def test_sine():
     result = calc.sine(90)
     assert result == 1
 
 def test_cosine():
-    calc = Calculator()
     result = calc.cosine(60)
     assert result == 0.5
 
 def test_tangent():
-    calc = Calculator()
     result = calc.tangent(45)
     assert result == 1
 
 def test_log():
-    calc = Calculator()
     result = calc.log10(100)
     assert result == 2
 
 def test_ln():
-    calc = Calculator()
     result = calc.ln(1)
     assert result == 0
