85b5e501dab99bd6829c4a8af3274cc9bf9f4389
ejshakman <81797140+ejshakman@users.noreply.github.com>
2024-06-01 22:28:00 +0000

    Set up GUI files for TDD

---
+++ tests/test_gui.py
@@ -0,0 +1,18 @@
+# tests/test_gui.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+# Display screen initialization
+
+# Display screen updates
+
+# Numeric buttons
+
+# Operator buttons
+
+# Equal button
+
+# Clear all button
+
+# Clear button
--- tests/test_gui_clear_display.py
+++
@@ -1,4 +0,0 @@
-# tests/test_gui_clear_display.py
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
