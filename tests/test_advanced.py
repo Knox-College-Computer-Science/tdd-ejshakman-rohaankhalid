dc00e7fe5cd972eea632f66916ba61fce230c771
rohaankhalid <rohaankhalid10@gmail.com>
2024-05-31 19:00:21 -0500

    renaming/modified airthmetic tests for structure

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,26 +1,22 @@
 import pytest
 from src.calc import Calculator
 
-def setup_function():
-    global calc
-    calc = Calculator()
+@pytest.fixture(scope="module")
+def calc():
+    return Calculator()
 
-def test_sine():
+def test_sine(calc):
     result = calc.sine(90)
     assert result == 1
 
-def test_cosine():
+def test_cosine(calc):
     result = calc.cosine(60)
     assert result == 0.5
 
-def test_tangent():
+def test_tangent(calc):
     result = calc.tangent(45)
     assert result == 1
 
-def test_log():
+def test_log(calc):
     result = calc.log10(100)
     assert result == 2
-
-def test_ln():
-    result = calc.ln(1)
-    assert result == 0
