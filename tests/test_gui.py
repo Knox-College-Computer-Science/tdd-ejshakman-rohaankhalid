e599ab15096a4d04f07cd790cc0e115ebcc2146b
ejshakman <81797140+ejshakman@users.noreply.github.com>
2024-06-02 23:56:30 -0500

    Make GUI tests match implementation

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -12,55 +12,55 @@ def gui():
 
 # Display screen initialization
 def test_display_initialization(gui):
-    assert gui.get_display_text() == ""
+    assert gui.current_input() == ""
 
 # Display screen update
 def test_display_update(gui):
-    gui.press_button('1')
-    gui.press_button('+')
-    gui.press_button('2')
+    gui.on_button_click('1')
+    gui.on_button_click('+')
+    gui.on_button_click('2')
     
-    assert gui.get_display_text() == "1+2"
+    assert gui.current_input() == "1+2"
 
 # Numeric buttons
 def test_numeric_buttons(gui):
-    gui.press_button('3')
-    gui.press_button('4')
-    gui.press_button('5')
+    gui.on_button_click('3')
+    gui.on_button_click('4')
+    gui.on_button_click('5')
 
-    assert gui.get_display_text() == "345"
+    assert gui.current_input() == "345"
 
 # Operator buttons
 def test_operator_buttons(gui):
-    gui.press_button('3')
-    gui.press_button('-')
-    gui.press_button('1')
-    gui.press_button('=')
+    gui.on_button_click('3')
+    gui.on_button_click('-')
+    gui.on_button_click('1')
+    gui.on_button_click('=')
 
-    assert gui.get_display_text() == "2"
+    assert gui.current_input() == "2"
 
 # Equal button
 def test_equal_button(gui):
-    gui.press_button('7')
-    gui.press_button('-')
-    gui.press_button('3')
-    gui.press_button('=')
+    gui.on_button_click('7')
+    gui.on_button_click('-')
+    gui.on_button_click('3')
+    gui.on_button_click('=')
 
-    assert gui.get_display_text() == "4"
+    assert gui.current_input() == "4"
 
 # Clear all button
 def test_clear_all_button(gui):
-    gui.press_button('6')
-    gui.press_button('7')
-    gui.press_button('C')
+    gui.on_button_click('6')
+    gui.on_button_click('7')
+    gui.on_button_click('C')
 
-    assert gui.get_display_text() == ""
+    assert gui.current_input() == ""
 
 # Clear button
 def test_clear_button(gui):
-    gui.press_button('9')
-    gui.press_button('0')
-    gui.press_button('CE')
+    gui.on_button_click('9')
+    gui.on_button_click('0')
+    gui.on_button_click('CE')
 
-    assert gui.get_display_text() == "9"
+    assert gui.current_input() == "9"
     
